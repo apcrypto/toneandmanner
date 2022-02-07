e1bf38de0f8faaacad458963ab859ed599a3881d
Andy Purbrick <andypurbrick@Andys-MacBook-Air.local>
2022-02-07 11:35:38 +0000

    reinstated onload event

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ var image = document.querySelector("#back1");
 var footer = document.querySelector(".footer");
 var copyright = document.querySelector(".copyright");
 
+window.addEventListener("load", (event) => {
+  setFooterHeight();
+});
+
 function setFooterHeight() {
   footer.style.top = image.clientHeight - 40 + "px";
   copyright.style.top = image.clientHeight - 100 + "px";
 }
-
-setFooterHeight();
 
 window.addEventListener("resize", setFooterHeight);
